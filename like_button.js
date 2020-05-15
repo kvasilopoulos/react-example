738863dd42838fe71d89fd6b93f5b4dcbb99abd4
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-05-15 18:03:20 +0100

    added button

--- like_button.js
+++ like_button.js
@@ -14,10 +14,3 @@ class LikeButton extends React.Component {
 }
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
-
-
-<DropdownButton id="dropdown-basic-button" title="Dropdown button">
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-</DropdownButton>
